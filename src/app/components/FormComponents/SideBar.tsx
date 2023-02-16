a11bc00e4464c55cb03fed400f8b2805ba1f2534
sameera222 <100690618+sameera222@users.noreply.github.com>
2023-02-16 16:38:28 +0530

    added sidebar to the dashboard screen

---
+++ src/app/components/FormComponents/SideBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="flex flex-col items-start p-5 h-screen w-72">
+      <div>Masters</div>
+      <div>Invite Code</div>
+      <div>Waiting Lists</div>
+      <div>Users</div>
+      <div>Platforms</div>
+      <div>Boarding Screens</div>
+      <div>Task Types</div>
+      <div>Retailers</div>
+      <div>Compaign Wizard</div>
+      <div>Compaigns</div>
+      <div>Reports</div>
+    </div>
+  );
+};
+export default SideBar;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,16 @@
+import SideBar from "@/app/components/FormComponents/SideBar";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex md:flex md:flex-grow flex-row justify-between space-x-1 bg-white w-full h-16 p-5">
-      <div className="text-black text-bold">Cuba</div>
-      <div className="text-black text-bold">Admin</div>
+    <div>
+      <div className="flex md:flex md:flex-grow flex-row justify-between space-x-1 bg-pink-300 w-full h-16 p-5">
+        <div className="text-black font-bold text-2xl">Cuba</div>
+        <div className="text-black font-bold text-2xl">Admin</div>
+      </div>
+      <div className="flex flex-col items-start bg-gray-300 w-72 h-full">
+        <SideBar />
+      </div>
     </div>
   );
 };
