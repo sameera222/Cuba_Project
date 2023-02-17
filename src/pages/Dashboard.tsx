9f339d254bf388a2e8c4eac51cef400204686a7c
sameera222 <100690618+sameera222@users.noreply.github.com>
2023-02-17 18:13:38 +0530

    added dashboard screen to the cuba

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,8 +8,13 @@ const Dashboard = () => {
         <div className="text-black font-bold text-2xl">Cuba</div>
         <div className="text-black font-bold text-2xl">Admin</div>
       </div>
-      <div className="flex flex-col items-start bg-gray-300 w-72 h-full">
-        <SideBar />
+      <div className="flex flex-row w-full h-full">
+        <div className="flex flex-col items-start bg-gray-300 w-72 h-full">
+          <SideBar />
+        </div>
+        <div className="flex flex-row items-start justfy-center w-full h-screen bg-gray-400 p-5">
+          <p>Welcome to the Dashboard, Dashboard will be ready Later</p>
+        </div>
       </div>
     </div>
   );
