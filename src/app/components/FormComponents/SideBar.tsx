b2d0ed102d349f7367e0d626d1e537972146da06
sameera222 <100690618+sameera222@users.noreply.github.com>
2023-02-17 19:26:17 +0530

    added react icons to the sidebar

--- src/app/components/FormComponents/SideBar.tsx
+++ src/app/components/FormComponents/SideBar.tsx
@@ -1,19 +1,93 @@
 import React from "react";
+import { GrHome } from "react-icons/gr";
+import { FaPencilRuler } from "react-icons/fa";
+import { MdCable } from "react-icons/md";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { HiUsers } from "react-icons/hi";
+import { RiShoppingBasketLine } from "react-icons/ri";
+import { SlScreenDesktop } from "react-icons/sl";
+import { BsCheck2Square } from "react-icons/bs";
+import { BsHexagon } from "react-icons/bs";
+import { TbBoxModel2 } from "react-icons/tb";
+import { RiListCheck } from "react-icons/ri";
+import { FaGripLinesVertical } from "react-icons/fa";
 
 const SideBar = () => {
   return (
     <div className="flex flex-col items-start p-5 h-screen w-72">
-      <div>Masters</div>
-      <div>Invite Code</div>
-      <div>Waiting Lists</div>
-      <div>Users</div>
-      <div>Platforms</div>
-      <div>Boarding Screens</div>
-      <div>Task Types</div>
-      <div>Retailers</div>
-      <div>Compaign Wizard</div>
-      <div>Compaigns</div>
-      <div>Reports</div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <GrHome size={20} />
+        </span>
+        Dashboard
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <FaPencilRuler size={20} />
+        </span>
+        Masters
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <MdCable />
+        </span>
+        Invite Code
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <AiOutlineClockCircle />
+        </span>
+        Waiting Lists
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <HiUsers />
+        </span>
+        Users
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <RiShoppingBasketLine />
+        </span>
+        Platforms
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <SlScreenDesktop />
+        </span>
+        Boarding Screens
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <BsCheck2Square />
+        </span>
+        Task Types
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <BsHexagon />
+        </span>
+        Retailers
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        {" "}
+        <span className="mr-3">
+          <TbBoxModel2 />
+        </span>
+        Compaign Wizard
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <RiListCheck />
+        </span>
+        Compaigns
+      </div>
+      <div className="flex flex-row mr-5 py-2">
+        <span className="mr-3">
+          <FaGripLinesVertical />
+        </span>
+        Reports
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,25 @@
 import SideBar from "@/app/components/FormComponents/SideBar";
 import React from "react";
+import { CgTikcode } from "react-icons/cg";
 
 const Dashboard = () => {
   return (
     <div>
-      <div className="flex md:flex md:flex-grow flex-row justify-between space-x-1 bg-pink-300 w-full h-16 p-5">
-        <div className="text-black font-bold text-2xl">Cuba</div>
+      <div className="flex md:flex md:flex-grow flex-row justify-between items-center bg-white w-full h-16 p-5">
+        <div className="text-black font-bold text-2xl mr-5 flex flex-row">
+          Cuba
+          <span className="ml-60">
+            <CgTikcode size={30} />
+          </span>
+        </div>
+
         <div className="text-black font-bold text-2xl">Admin</div>
       </div>
       <div className="flex flex-row w-full h-full">
-        <div className="flex flex-col items-start bg-gray-300 w-72 h-full">
+        <div className="flex flex-col items-start bg-white w-72 h-full">
           <SideBar />
         </div>
-        <div className="flex flex-row items-start justfy-center w-full h-screen bg-gray-400 p-5">
+        <div className="flex flex-row items-start justfy-center w-full h-screen bg-gray-100 p-5">
           <p>Welcome to the Dashboard, Dashboard will be ready Later</p>
         </div>
       </div>
